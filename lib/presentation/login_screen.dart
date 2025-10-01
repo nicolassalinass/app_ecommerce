4526dc6372080bdc7528fa51412780f7cee87e79
Ricardo Montiel <rickymontiel025@gmail.com>
2025-10-01 16:59:11 -0300

    Login Funcional

--- lib/presentation/login_screen.dart
+++ lib/presentation/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app_ecomerce/presentation/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,53 +9,123 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  // Creamos los controladores de los campos de texto
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.indigo,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "INICIAR SESIÓN",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 28,
-                color: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+              Text(
+                "INICIAR SESIÓN",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                  color: Colors.white,
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            Text("Email o telefono", style: TextStyle(color: Colors.white)),
-            SizedBox(
-              width: 300,
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(50)),
+              SizedBox(height: 60),
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Email o telefono",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  prefixIcon: Icon(Icons.person, color: Colors.white),
-                  // prefixIconColor: Colors.white      ------- Forma alternativa de relizar el cambio de color del icono
-                  label: Text("Correo"),
-                  labelStyle: TextStyle(color: Colors.white),
-                  hintText: "ejemplo123@gmail.com",
-                  hintStyle: TextStyle(color: Colors.white),
+                  SizedBox(height: 10),
+                  SizedBox(
+                    width: double.infinity,
+                    child: TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                        ),
+                        prefixIcon: Icon(Icons.person),
+                        // prefixIconColor: Colors.white      ------- Forma alternativa de relizar el cambio de color del icono
+                        // label: Text("Correo"),
+                        // labelStyle: TextStyle(color: Colors.white),
+                        hintText: "ejemplo123@gmail.com",
+                        // hintStyle: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "Contraseña",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  SizedBox(
+                    width: double.infinity,
+                    child: TextField(
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        // icon: Icon(Icons.phone),
+                        prefixIcon: Icon(Icons.lock),
+                        // label: Text("Contraseña"),
+                        hintText: "12345678.ejemplo",
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 50),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    final email = emailController.text.trim();
+                    final password = passwordController.text.trim();
+                    if (email.isEmpty || password.isEmpty) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("Complete todos los campos!")),
+                      );
+                    } else {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) => HomeScreen()),
+                      );
+                    }
+                  },
+                  style: ButtonStyle(
+                    foregroundColor: WidgetStatePropertyAll(Colors.white),
+                    backgroundColor: WidgetStateProperty.resolveWith<Color>((
+                      Set<WidgetState> state,
+                    ) {
+                      if (state.contains(WidgetState.pressed)) {
+                        return const Color.fromARGB(255, 2, 141, 9);
+                      }
+                      return const Color.fromARGB(255, 63, 207, 34);
+                    }),
+                  ),
+                  child: Text(
+                    "Iniciar Sesión",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            SizedBox(
-              width: 300,
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  // icon: Icon(Icons.phone),
-                  prefixIcon: Icon(Icons.lock),
-                  label: Text("Contraseña"),
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
