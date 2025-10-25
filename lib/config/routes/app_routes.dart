6c111eac74ad1b6be372dd19d03ee2e203d64a0f
nicolassalinass <salinasnicolasalb@gmail.com>
2025-10-25 18:31:49 -0300

    Reestructuracion de carpetas

---
+++ lib/config/routes/app_routes.dart
@@ -0,0 +1,5 @@
+class AppRoutes {
+  static const String home = '../../features/home/ui/home_screen.dart';
+  static const String login = '../../features/auth/ui/login_screen.dart';
+  static const String cart = '../../features/cart/ui/cart_shopping_screen.dart';
+}
