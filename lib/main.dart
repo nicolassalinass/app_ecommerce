7abae35dcee0423acb3373bf03bf7a125bca59eb
nicolasssalinass <salinasnicolasalb@gmail.com>
2025-11-19 12:43:27 -0300

    Cambios en el main

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:app_ecomerce/features/auth/ui/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final RouteObserver<ModalRoute<void>> routeObserver = RouteObserver<ModalRoute<void>>();
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initDependencies();
@@ -21,6 +23,7 @@ class MyApp extends StatelessWidget {
       darkTheme: AppTheme.darkTheme,
       theme: AppTheme.lightTheme,
       themeMode: ThemeMode.system,
+      navigatorObservers: [routeObserver],
       home: LoginScreen(),
     );
   }
